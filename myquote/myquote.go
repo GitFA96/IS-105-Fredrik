e536371a768bef09eb915c171e9def7318686bef
GitFA96 <89194109+GitFA96@users.noreply.github.com>
2022-01-28 12:16:47 +0100

    testing av myqoute

--- myquote/myquote.go
+++ myquote/myquote.go
@@ -3,9 +3,12 @@ package myquote
 import "fmt"
 import "rsc.io/quote"
 
-func PrintQuotes() {
+func PrintQuotes1() {
 	fmt.Println(quote.Glass())
 	fmt.Println(quote.Go())
+}
+
+func PrintQuotes2() {
 	fmt.Println(quote.Hello())
 	fmt.Println(quote.Opt())
 }
